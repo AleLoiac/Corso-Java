96226b6792c123b122ea2e9ba6f88abcf8afc1cd
AleLoiac <alessandroloiac@gmail.com>
2022-09-15 12:29:13 +0200

    15/09

--- Es_11_Eccezioni/src/Es_4.java
+++ Es_11_Eccezioni/src/Es_4.java
@@ -1,7 +1,27 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class Es_4 {
     public static void main(String[] args) {
 
-
         int[] array = new int[5];
+
+        Scanner input;
+        int n = 0;
+        input = new Scanner(System.in);
+        System.out.println("Inserisci posizione:");
+        n = input.nextInt();
+
+        input = new Scanner(System.in);
+        System.out.println("Inserisci numero:");
+        int i = input.nextInt();
+
+        try {
+            array[n] = i;
+        }catch (Exception e){
+            System.out.println("Fuori dall'array");
+        }
+
+        System.out.println(Arrays.toString(array));
     }
 }
