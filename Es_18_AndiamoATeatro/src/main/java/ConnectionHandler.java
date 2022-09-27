4a4aa4ddb57cded056b5bf710156715a829ab25a
AleLoiac <alessandroloiac@gmail.com>
2022-09-27 17:49:35 +0200

    27/09

--- Es_18_AndiamoATeatro/src/main/java/ConnectionHandler.java
+++ Es_18_AndiamoATeatro/src/main/java/ConnectionHandler.java
@@ -1,4 +1,20 @@
+import java.sql.Connection;
+
 public class ConnectionHandler {
 
+    public static final String DB_URL = "jdbc:postgresql://localhost:5432/";
 
+    public final String connectionUrl;
+
+    private Connection connection;
+
+    public ConnectionHandler(String dbUrl, String dbName, String schema, String username, String password) throws ClassNotFoundException {
+        this.connectionUrl = dbUrl + dbName
+                + "?currentSchema=" + schema
+                + "&user=" + username
+                + "&password=" + password;
+
+        // Occorre conoscere il nome del driver e il suo package
+        Class.forName("org.postgresql.Driver");
+    }
 }
